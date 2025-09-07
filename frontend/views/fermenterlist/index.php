47e37437905148e693ec69bbf66726875ee005cc
Дмитрий Кожухарь <d.kozhuhar@gmail.com>
2025-09-07 23:30:19 +0500

    start

--- frontend/views/fermenterlist/index.php
+++ frontend/views/fermenterlist/index.php
@@ -6,8 +6,39 @@
  * Time: 14:27
  */
 
-
 $this->title = 'Управление пивобезакло';
 
-echo "111111";
+use common\models\Fermenter;
+
+
 ?>
+
+
+
+<?php
+$fermenter = new Fermenter();
+$fermenter->id = 1;
+$fermenter->name = 'ЦКТ 1';
+$fermenter->status = 0;
+?>
+
+
+<div class="container-fluid">
+    <div class="row">
+        <table>
+            <thead>
+            <th>ID</th>
+            <th>Name</th>
+            <th>Status</th>
+            </thead>
+            <tbody>
+            <tr>
+                <td><?= $fermenter->id;?></td>
+                <td><?= $fermenter->name;?></td>
+                <td><?= $fermenter->status;?></td>
+            </tr>
+            </tbody>
+        </table>
+
+    </div>
+</div>
