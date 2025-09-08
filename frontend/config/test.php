ecde1098768250f1f95f747ccccdc5bb23ea77df
Дмитрий Кожухарь <d.kozhuhar@gmail.com>
2025-09-08 12:32:27 +0500

    start

--- frontend/config/test.php
+++ frontend/config/test.php
@@ -13,3 +13,11 @@ return [
         ],
     ],
 ];
+
+
+/*
+ *
+php yii gii/crud  --modelClass=app\models\Material --controllerClass=app\controllers\MaterialController --searchModelClass=app\models\MaterialSearch --viewPath=@app/views/materials --interactive=0 --overwrite=1
+php yii gii/crud  --modelClass=app\models\Supplier --controllerClass=app\controllers\SupplierController --searchModelClass=app\models\SupplierSearch --viewPath=@app/views/suppliers --interactive=0 --overwrite=1
+php yii gii/crud  --modelClass=app\models\Uoms --controllerClass=app\controllers\UomsController      --searchModelClass=app\models\UomsSearch      --viewPath=@app/views/uoms      --interactive=0 --overwrite=1
+ */
